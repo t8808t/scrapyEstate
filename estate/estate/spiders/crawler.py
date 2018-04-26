b4e900a0bbfedb8220ede5d0bcddad88fd302cb8
t8808t <31009965+t8808t@users.noreply.github.com>
2018-04-26 17:00:24 +0800

    Update crawler.py

--- estate/estate/spiders/crawler.py
+++ estate/estate/spiders/crawler.py
@@ -9,14 +9,14 @@ class EstateSpider(Spider):
 
     def start_requests(self):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
-        base_urls = 'http://jjhygl.hzfc.gov.cn/webty/WebFyAction_getGpxxSelectList.jspx?gply=&wtcsjg=&jzmj=&ordertype=&fwyt=&hxs=&havepic=&xzqh=&starttime=&endtime=&keywords=&page=1&xqid=0'
+        base_urls = 'YourURL'
         response = requests.get(base_urls,headers =headers)
         respList = json.loads(response.text)
         reobj_Page = re.compile('href="javascript:void\(0\)" class="c" onclick="doPage\((\d+)\);return false;')  # 正则匹配出当前页码和总页码
         totalPage = reobj_Page.findall(respList['pageinfo'])[1]
         print("totalPage : "+totalPage)
         for nextPage in range(1, int(totalPage)):
-            absolute_next_page_url =  'http://jjhygl.hzfc.gov.cn/webty/WebFyAction_getGpxxSelectList.jspx?gply=&wtcsjg=&jzmj=&ordertype=&fwyt=&hxs=&havepic=&xzqh=&starttime=&endtime=&keywords=&page='+str(nextPage)+'&xqid=0'
+            absolute_next_page_url =  'YourURL?page='+str(nextPage)'
             yield Request(absolute_next_page_url, self.parse)
 
     def parse(self, response):
@@ -43,19 +43,3 @@ class EstateSpider(Spider):
         totalPage1 = reobj_Page.findall(respList['pageinfo'])[1]
         #print("当前页码为 : "+ str(currentPage))
         print(nextPage + " / " + totalPage1)
-
-
-
-
-
-    # def parse(self, response):
-    #      item = PbcdcItem()
-    #      dakuangjia = response.xpath('//*[@id="J_motoDataMain"]/div[1]/div[3]/ul/li')
-    #      for xiaokuangjia in dakuangjia:
-    #          item["name"] = xiaokuangjia.xpath('.//p/a//text()').extract_first()
-    #          item["price"] = xiaokuangjia.xpath('.//p/a//em//text()').extract_first()
-    #          yield item
-
-         # next_url =response.xpath('//a[text()="后页>"]/@href').extract_first()
-         # absolute_next_page_url = response.urljoin(next_url)
-         # yield Request(absolute_next_page_url)
